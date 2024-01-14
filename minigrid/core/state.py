7ffac0e0b7f2826a7262503ed0a5c7bbd73aeb67
sp <stefan.pranger@iaik.tugraz.at>
2024-01-14 13:09:23 +0100

    WIP further work in to_state for shield parsing

--- minigrid/core/state.py
+++ minigrid/core/state.py
@@ -1,4 +1,8 @@
 from dataclasses import dataclass, field
+from minigrid.core.constants import (
+    COLOR_NAMES,
+    IDX_TO_COLOR
+)
 
 @dataclass(frozen=True, eq=True)
 class Key:
@@ -30,4 +34,28 @@ class State:
     colAgent: int
     rowAgent: int
     viewAgent: int
-    adversaries: tuple
+    adversaries: tuple = field(default_factory=tuple)
+    balls: tuple = field(default_factory=tuple)
+
+def to_state(ints, booleans):
+    agentState = (ints["colAgent"], ints["rowAgent"], ints["viewAgent"])
+    ints = {key:int(value) for key, value in ints.items()}
+    booleans = {value: False if key == "!" else True for key, value in booleans.items()}
+    adversaries = tuple()
+    boxes = tuple()
+    balls = tuple()
+    keys = tuple()
+    for color in COLOR_NAMES:
+        color = color.capitalize()
+        if "col" + color in ints:
+            adversaries += (Adversary(color, ints["col"+color], ints["row"+color], ints["view"+color]),)
+        if "col" + color + "Ball" in ints:
+            identifier = color + "Ball"
+            balls += (Ball(color, ints["col"+identifier], ints["row"+identifier]),)
+        if "col" + color + "Box" in ints:
+            pass
+        if "col" + color + "Key" in ints:
+            pass
+
+
+    return State(*agentState, adversaries, balls)
